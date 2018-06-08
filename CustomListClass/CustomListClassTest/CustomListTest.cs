5aaa6542146d231211d64a747d2805f37cc1bcd9
Garrett <davisgarrett21@yahoo.com>
2018-06-07 20:55:05 -0500

    Added a second test for the Add method that checked if the actual value of index 0 was equal to the expected value

--- CustomListClass/CustomListClassTest/CustomListTest.cs
+++ CustomListClass/CustomListClassTest/CustomListTest.cs
@@ -31,8 +31,33 @@ namespace CustomListClassTest
 
             //assert
             Assert.AreEqual(expectedCount, actualCount);
-            
+
         }
 
-        
-}
+        [TestMethod]
+        public void Add_Int_To_List_Check_Specific_Index()
+        {
+            //arrange
+            CustomList<int> list = new CustomList<int>();
+            int expectedValue = 20;
+            int valueToAdd = 20;
+
+            //check to see that valuetoAdd which is 20, is at index 0.
+
+
+
+
+            //act
+            list.Add(valueToAdd);
+            int actualValue = list[0];
+
+
+
+
+
+
+
+            //assert
+            Assert.AreEqual(expectedValue, actualValue);
+        } 
+
